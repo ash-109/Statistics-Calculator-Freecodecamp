84e48d90b4c7f31ebd6b2fdf983a9fe8219d669d
Ashvin Antyah <a.antyah@gmail.com>
2024-07-05 18:29:24 +0200

    update html & js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Calcul de la moyenne
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length; // fonction fléchée retour implicite
 
 
@@ -7,7 +9,50 @@ const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+  const mean = getMean(numbers)
+
+  document.querySelector("#mean").textContent = mean; // afficher la valeur de mean dans le html
 };
+
+
+// calculer la mediane
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+
+}
+
+
+
+// savoir si la longueur d'un tableau est paire ou impraire (en utilisant le modulo %)
+
+/* const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+// check if array length is even
+let isEven= testArr2.length % 2 === 0
+console.log(isEven);
+
+// check if array length is odd
+arr.length % 2 === 1;
+
+!!!! Si le reste est 0, le nombre est pair. Si le reste est 1, le nombre est impair. !!!!
+ /*
+
+// Trouver la mediane d'un tableau d'une longueur impaire
+// arr[Math.floor(arr.length / 2)] 
+// le résultat de la division d’un nombre impair par 2 sera une décimale. Math.floor arrondira au nombre entier le plus proche.
+let oddListMedian = testArr1[Math.floor(testArr1.length /2 )]
+console.log(oddListMedian);
+
+
+// Trouver la mediane d'un tableau d'une longueur paire 
+// Pour trouver la médiane d’une liste pair de nombres, vous devez trouver les deux nombres intermédiaires et calculer la moyenne de ces nombres.
+// first middle number  ----> arr[arr.length / 2];
+// second middle number ----> arr[(arr.length / 2) - 1];
+
+/*const firstMiddleNumber = testArr2[testArr2.length / 2];
+const secondMiddleNumber = testArr2[(testArr2.length / 2) - 1];
+ let evenListMedian = getMean([firstMiddleNumber,secondMiddleNumber])
+console.log(evenListMedian);/*
 
 // Calculer la moyenne
   // const sum = array.reduce((acc, el) => acc + el, 0); // somme de tout les elements
