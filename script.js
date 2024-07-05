b3218cfc8ad1def17eb490617c2d839bbc306a06
Ashvin Antyah <a.antyah@gmail.com>
2024-07-05 11:15:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length; // fonction fléchée retour implicite
+
+
+
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
-  const numbers = array.map(el=> Number(el)).filter(el => !isNaN(el));
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
 };
 
 // Calculer la moyenne
- function getMean(array) {
-  const sum= array.reduce()
- }
+  // const sum = array.reduce((acc, el) => acc + el, 0); // somme de tout les elements
+  // const mean = sum/array.length // moyenne = somme / nb d'elements
+  // return mean
+
+  
